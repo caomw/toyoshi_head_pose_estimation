a3b3b0c00c03caca6f4a320a4a8b03c9b26aaf16
Masahiko Toyoshi <m.yoshi.1700@gmail.com>
2012-11-09 09:57:22 +0900

    First commit

---
+++ demo/head_pose_estimation_demo.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/masahiko/Programs/head_pose_estimation/CRForestEstimator.cpp" "/Users/masahiko/Programs/head_pose_estimation/demo/head_pose_estimation_demo.dir/Users/masahiko/Programs/head_pose_estimation/CRForestEstimator.cpp.o"
+  "/Users/masahiko/Programs/head_pose_estimation/CRTree.cpp" "/Users/masahiko/Programs/head_pose_estimation/demo/head_pose_estimation_demo.dir/Users/masahiko/Programs/head_pose_estimation/CRTree.cpp.o"
+  "/Users/masahiko/Programs/head_pose_estimation/demo/gl_camera.cpp" "/Users/masahiko/Programs/head_pose_estimation/demo/head_pose_estimation_demo.dir/gl_camera.cpp.o"
+  "/Users/masahiko/Programs/head_pose_estimation/demo/main.cpp" "/Users/masahiko/Programs/head_pose_estimation/demo/head_pose_estimation_demo.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  ".."
+  "/usr/include/ni"
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
